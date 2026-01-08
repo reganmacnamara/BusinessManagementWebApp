da68d8ef3f72ce07c5f09c8a326f170a5bf95725
Regan Macnamara 20069849 <Reganmac3@gmail.com>
2026-01-08 14:32:31 +1100

    Base Transaction

---
+++ Data/Transactions/Entities/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using InvoiceAutomationWebApp.Data.Clients.Entities;
+
+namespace InvoiceAutomationWebApp.Data.Transactions.Entities
+{
+
+    public class Transaction
+    {
+        public long TransactionID { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public string TransactionType { get; init; }
+
+        public DateOnly DueDate { get; set; }
+
+        public Client Client { get; set; } = null!;
+
+        public decimal GrossValue { get; set; }
+
+        public decimal TaxValue { get; set; }
+
+        public decimal NetValue { get; set; }
+
+        public decimal OffsetValue { get; set; }
+    }
+
+}
